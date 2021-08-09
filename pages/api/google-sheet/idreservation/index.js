972f2f43ca381f87f30edb1c2d3fe1ec1cc98053
José Juan Téllez Guzmán <jjtellezg@gmail.com>
2021-08-09 13:43:01 -0500

    update google sheet 35

--- pages/api/google-sheet/idreservation/index.js
+++ pages/api/google-sheet/idreservation/index.js
@@ -18,6 +18,8 @@ export default handler
 
     if (req.userName === "Juan Tellez") {
       try {
+        console.log("Datos: ", reservationUpdate);
+
         const { sheetGoogle } = await callApiGoogleSheet(
           NEXT_PUBLIC_SPREADSHEET_ID_MARIACHON,
           NEXT_PUBLIC_SHEET_ID
@@ -29,10 +31,85 @@ export default handler
 
         const filteredRow = (await getReservationById[0]._rowNumber) - 2;
 
+        if (reservationUpdate.createdAt) {
+          const creado = new Date(reservationUpdate.createdAt);
+
+          sheetGoogle[filteredRow].fecha_creacion = creado.toLocaleDateString(
+            "es-MX",
+            options
+          );
+        }
+
+        if (reservationUpdate.reservationId) {
+          sheetGoogle[filteredRow].reservationId =
+            await reservationUpdate.reservationId;
+        }
+
+        if (reservationUpdate.date) {
+          const date = new Date(reservationUpdate.date);
+
+          sheetGoogle[filteredRow].fecha_y_hora =
+            date.toLocaleDateString("es-MX", options) +
+            ", " +
+            date.toLocaleTimeString();
+        }
+
+        if (reservationUpdate.mariachiData.nameMariachi) {
+          sheetGoogle[filteredRow].mariachi = await reservationUpdate
+            .mariachiData.nameMariachi;
+        }
+
+        if (reservationUpdate.address) {
+          sheetGoogle[filteredRow].direccion = await reservationUpdate.address;
+        }
+
+        if (reservationUpdate.client.nameClient) {
+          sheetGoogle[filteredRow].cliente = await reservationUpdate.client
+            .nameClient;
+        }
+
+        if (reservationUpdate.client.phone) {
+          sheetGoogle[filteredRow].tel = await reservationUpdate.client.phone;
+        }
+
+        if (reservationUpdate.client.email) {
+          sheetGoogle[filteredRow].email = await reservationUpdate.client.email;
+        }
+
+        if (reservationUpdate.service) {
+          sheetGoogle[filteredRow].servicio = await reservationUpdate.service;
+        }
+
+        if (reservationUpdate.qty) {
+          sheetGoogle[filteredRow].cantidad = await reservationUpdate.qty;
+        }
+
+        if (reservationUpdate.price) {
+          sheetGoogle[filteredRow].precio = await (reservationUpdate.price *
+            reservationUpdate.qty);
+        }
+
+        if (reservationUpdate.deposit) {
+          sheetGoogle[filteredRow].deposito = await reservationUpdate.deposit;
+        }
+
+        if (reservationUpdate.message) {
+          sheetGoogle[filteredRow].mensaje = await reservationUpdate.message;
+        }
+
+        if (reservationUpdate.playlist) {
+          sheetGoogle[filteredRow].Lista_de_canciones =
+            await reservationUpdate.playlist;
+        }
+
         if (reservationUpdate.status) {
           sheetGoogle[filteredRow].status = await reservationUpdate.status;
         }
 
+        if (reservationUpdate.coordinatorData.userName) {
+          sheetGoogle[filteredRow].coordinator = await reservationUpdate
+            .coordinatorData.userName;
+        }
         await sheetGoogle[filteredRow].save();
 
         res.status(200).json({
